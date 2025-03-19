813387a2fa8a44e7898b922aa13528e781c77ce5
Pontax02 <ppontanilla07@gmail.com>
2025-03-19 10:31:27 +0100

    :sparkles: feat(Sneaker): Implementar los metodos de la interfaz a la clase sneaker

--- StockX/src/main/java/edu/estatuas/Sneaker.java
+++ StockX/src/main/java/edu/estatuas/Sneaker.java
@@ -3,6 +3,9 @@ package edu.estatuas;
 public class Sneaker implements Item {
     public final String style;
     public final String name;
+    public int bid;
+    public int ask;
+    public int sale;
 
     public Sneaker(String style, String name) {
         this.style = style;
@@ -11,14 +14,29 @@ public class Sneaker implements Item {
     }
 
     private String getStyle(){
-        return style;
+        return this.style;
     }
     private String getName(){
-        return name;
+        return this.name;
     }
     @Override
+    public int getAsk(){
+        return this.ask;
+    }
+    @Override
+    public int getBid(){
+        return this.bid;
+    }
+    @Override
+    public int getSale(){
+        return this.sale;
+    }
+
+
+
+    @Override
     public String toString() {
-        return getName() + " \n" + getStyle();
+        return this.getName() + " \n" + this.getStyle();
 
     }
 }
