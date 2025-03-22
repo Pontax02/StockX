2486d223979691813ab7bbdebad6b27c6b416580
Pontax02 <ppontanilla07@gmail.com>
2025-03-22 17:42:43 +0100

    :white_check_mark: test(SneakerTest): Añadir casos tests

--- StockX/src/test/java/edu/estatuas/SneakerTest.java
+++ StockX/src/test/java/edu/estatuas/SneakerTest.java
@@ -22,6 +22,33 @@ public class SneakerTest {
         assertEquals("Jordan 1 Retro High Dark Mocha",snk.getName());
 
     }
+    @Test
+    public void addOfferBid() {
+        Sneaker sneaker = new Sneaker("555088-105", "Jordan 1");
+        Bid bid = new Bid("9.5", 282);
+        sneaker.add(bid);
+        assertEquals(1, sneaker.offers().size());
+    }
+    @Test
+    public void addOfferAsk() {
+        Sneaker sneaker = new Sneaker("555088-105", "Jordan 1");
+        Ask ask = new Ask("15", 288);
+        sneaker.add(ask);
+        sneaker.add(new Ask("13", 333));
+        sneaker.add(new Ask("14", 340));
+        sneaker.add(new Ask("13", 341));
+        assertEquals(4, sneaker.offers().size());
+    }
+
+    @Test
+    public void addOffer() {
+        Sneaker sneaker = new Sneaker("555088-105", "Jordan 1");
+        sneaker.add(new Bid("9.5", 282));
+        sneaker.add(new Ask("15", 288));
+        Offer ask = new Ask("13", 333);
+        sneaker.add(ask);
+        assertEquals(3, sneaker.offers().size());
+    }
 
 
 }
