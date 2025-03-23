006361d3361be50166ce69bc702911db2617b906
Pontax02 <ppontanilla07@gmail.com>
2025-03-23 13:31:29 +0100

    :ambulance: fix(Size): corregir el metodo filter

--- StockX/src/main/java/edu/estatuas/Size.java
+++ StockX/src/main/java/edu/estatuas/Size.java
@@ -5,12 +5,17 @@ import java.util.stream.Collectors;
 
 public class Size implements Criteria{
 
+    public String size;
+
+    Size(String size){
+        this.size=size;
+    }
 
     @Override
     public List<Offer> checkCriteria(Item sneaker) {
         return sneaker.offers()
                 .stream()
-                .filter(o -> o instanceof Size )
+                .filter(o -> o.size() == size )
                 .collect(Collectors.toList());
 
     }
